d6e132ef06037c27554a627619c544d2599cd713
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-03-24 13:21:43 +0530

    Time: 5 ms (79.68%), Space: 45.9 MB (82.97%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int rem;
+        int temp = x;
+        while(x>0){
+            rem = x%10;
+            rev = rev * 10 + rem;
+            x /= 10;
+        }
+        return temp == rev;
+    }
+}
