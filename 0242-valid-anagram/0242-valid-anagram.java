2f734db343a8985ed1915a7f2062472c7475df74
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-04-07 13:00:51 +0530

    Time: 28 ms (5.49%), Space: 46.5 MB (36.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        TreeMap<Character, Integer> map1 = new TreeMap<>();
+        TreeMap<Character, Integer> map2 = new TreeMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (map1.get(ch) == null) {
+                map1.put(ch, 1);
+            }
+
+            else {
+                map1.put(ch, map1.get(ch) + 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (map2.get(ch) == null) {
+                map2.put(ch, 1);
+            }
+
+            else {
+                map2.put(ch, map2.get(ch) + 1);
+            }
+        }
+
+        return map1.equals(map2);
+    }
+}
