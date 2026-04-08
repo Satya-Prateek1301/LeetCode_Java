2385debfb7ce6c832f501f4900a51d603def7c74
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-04-08 11:26:03 +0530

    Time: 0 ms (100%), Space: 42.3 MB (22.12%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // return (int) Math.sqrt(n);
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            count++;
+        }
+        return count;
+    }
+}
