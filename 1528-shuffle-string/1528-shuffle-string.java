a326cf86cc66c8206f2b91d83e616fe01961f6dd
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-04-09 13:37:31 +0530

    Time: 0 ms (100%), Space: 45.6 MB (62.83%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int len = s.length();
+        char[] arr = new char[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[indices[i]] = s.charAt(i);
+        }
+        return new String(arr);
+    }
+}
