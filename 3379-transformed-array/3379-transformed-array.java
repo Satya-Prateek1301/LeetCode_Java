e4222a440e03ae2636571ee893a3089203abfaac
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-04-10 21:56:16 +0530

    Time: 1 ms (99.37%), Space: 47 MB (14.02%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0 ; i < n ; i++){
+            if(nums[i] == 0){
+                res[i] = nums[i];
+            }
+            else{
+                int idx = (i + ( nums[i] % n) + n) % n;
+                res[i] = nums[idx];
+            }
+        }
+        return res;
+    }
+}
