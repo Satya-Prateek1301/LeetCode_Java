bde1c7dd4a3c9ae33f306c8828f9013934560bfc
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-04-10 22:07:48 +0530

    Time: 7 ms (46.82%), Space: 47 MB (42.56%) - LeetHub

---
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : bulbs){
+            if(set.contains(i)){
+                set.remove(i);
+            }
+            else{
+                set.add(i);
+            }
+        }
+        List<Integer> res = new ArrayList<>(set);
+        Collections.sort(res);
+        return res;
+    }
+}
