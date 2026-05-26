c8c10ecbbf451253d73748b995b61b6949da3c6b
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-05-26 12:09:25 +0530

    Time: 1 ms (100%), Space: 44.1 MB (83.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,29 +1,32 @@
 class Solution {
+    public char toLower(char ch3){
+        if((ch3 >= 'A' && ch3 <= 'Z')){
+            ch3 = (char)(ch3 + 32);
+        }
+        return ch3;
+    }
     public boolean isPalindrome(String s) {
-        int lenS = s.length();
+        // String satya = s;
+        int len = s.length();
         int left = 0;
-        int right = lenS - 1;
-
-        while( left < right){
-            char ch1 = s.charAt(left);
-            char ch2 = s.charAt(right);
-            // Character.toLowerCase(ch1);
-            // Character.toLowerCase(ch2);
-            if(!Character.isLetterOrDigit(ch1)){
+        int right = len - 1;
+        while(left < right){
+            char leftCh = s.charAt(left);
+            char rightCh = s.charAt(right);
+            if(!((leftCh >= 'A' && leftCh <= 'Z') || (leftCh >= 'a' && leftCh <= 'z') || (leftCh >= '0' && leftCh <= '9'))){
                 left++;
                 continue;
             }
-
-            if(!Character.isLetterOrDigit(ch2)){
+            if(!((rightCh >= 'A' && rightCh <= 'Z') || (rightCh >= 'a' && rightCh <= 'z') || (rightCh >= '0' && rightCh <= '9'))){
                 right--;
                 continue;
             }
-            if(Character.toLowerCase(ch1) != Character.toLowerCase(ch2)){
+            if(toLower(leftCh) != toLower(rightCh)){
                 return false;
             }
-                left++;
-                right--;
-            }
-        return true;
+            left++;
+            right--;
         }
+        return true;
     }
+}
