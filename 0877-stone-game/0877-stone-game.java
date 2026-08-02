e37f74cb8a3e53541d5ddd160e4857cd1c5d2166
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-08-03 01:04:03 +0530

    Time: 0 ms (100%), Space: 43.3 MB (25.83%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                evenSum += piles[i];
+            } else {
+                oddSum += piles[i];
+            }
+        }
+        if(evenSum != oddSum){
+            return true
+            ;
+        }
+        return false;
     }
 }
