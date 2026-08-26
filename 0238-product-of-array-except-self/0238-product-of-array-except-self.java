b0fb6d11ff384af818018a5b6517367b4cccb2d9
Satya-Prateek1301 <satya.prateek13@gmail.com>
2026-08-27 00:35:59 +0530

    Time: 2 ms (93.56%), Space: 72.3 MB (6.57%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,17 +3,14 @@ class Solution {
         int prefix = 1;
         int postfix = 1;
         int[] res = new int[nums.length];
-
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0 ; i < nums.length ; i++){
             res[i] = prefix;
             prefix *= nums[i];
         }
-
-        for(int i = nums.length-1; i >= 0; i--){
+        for(int i = nums.length - 1 ; i >= 0 ; i--){
             res[i] *= postfix;
             postfix *= nums[i];
         }
-
         return res;
     }
 }
